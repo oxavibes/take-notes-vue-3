84ee9e3dd52d1c162aff73cfb6e8ee4b0cdc0400
oxavibes <bozzistef@gmail.com>
2022-07-19 01:46:45 -0300

    install Pinia Plugin Persisted State

--- src/main.js
+++ src/main.js
@@ -1,9 +1,16 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
 
 import "vue-toast-notification/dist/theme-sugar.css";
 
 import App from "./App.vue";
 import router from "@/router";
 
-createApp(App).use(router).use(createPinia()).mount("#app");
+const pinia = createPinia().use(piniaPluginPersistedstate);
+
+const app = createApp(App);
+
+app.use(router).use(pinia);
+
+app.mount("#app");
