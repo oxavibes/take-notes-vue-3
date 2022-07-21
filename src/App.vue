fad04786171a33b52ec3502110de6e4ca80c7fc4
oxavibes <bozzistef@gmail.com>
2022-07-21 07:35:27 -0300

    Change layout and design

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,38 @@
 <script setup>
 import { ref } from "vue";
+import { useBreakpoints } from "@vueuse/core";
 
 import NavBar from "@/components/layout/NavBar.vue";
 import Footer from "@/components/layout/Footer.vue";
 
+const breakpoints = useBreakpoints({
+  mobile: 768,
+  tablet: 769,
+  desktop: 1024,
+  widescreen: 1216,
+  fullhd: 1408,
+});
+
+const desktopAndGreater = breakpoints.greater("desktop");
 </script>
 
 <template>
   <div class="container is-max-desktop is-flex is-flex-direction-column site">
-    <header class="block">
-      <div class="box">
-        <NavBar />
-      </div>
+    <header class="block box" :class="desktopAndGreater ? 'mt-2' : ''">
+      <NavBar />
     </header>
 
     <main class="is-flex-grow-1">
       <RouterView />
     </main>
 
-    <Footer />
+    <Footer :class="desktopAndGreater ? 'mb-4' : ''" />
   </div>
 </template>
 
 <style>
 @import "bulma/css/bulma.min.css";
-@import 'animate.css';
+@import "animate.css";
 
 .site {
   min-height: 100vh;
